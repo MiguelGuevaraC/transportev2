c1f516a3fd444bb08ec5e7daddbdc7190a1dcdc0
MiguelGuevaraC <mguevaracaj@unprg.edu.pe>
2025-02-06 12:40:45 -0500

    feat: Agregar seeder para setearle todos los accesos a adminsitrador

--- database/seeders/SetAccesAdmin.php
+++ database/seeders/SetAccesAdmin.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class SetAccesAdmin extends Seeder
@@ -15,8 +16,10 @@ class SetAccesAdmin extends Seeder
      */
     public function run()
     {
-        Role::find(1)->syncPermissions([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25,26,27]);
-        Role::find(2)->syncPermissions([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25,26,27]);
-       
+        $permissions = Permission::all();
+        // Obtener los IDs de los permisos
+        $permissionIds = $permissions->pluck('id')->toArray();
+        Role::find(1)->syncPermissions($permissionIds);
+        Role::find(2)->syncPermissions($permissionIds);
     }
 }
