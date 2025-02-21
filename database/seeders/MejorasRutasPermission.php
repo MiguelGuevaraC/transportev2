85dcfa4e2805b859917edd56a33179b5e9f34719
MiguelGuevaraC <mguevaracaj@unprg.edu.pe>
2025-02-21 07:10:37 -0500

    feat: agregar nuevo modulo y organizar tarifas unidades productos doc almacen en programaciones

--- database/seeders/MejorasRutasPermission.php
+++ database/seeders/MejorasRutasPermission.php
@@ -1,8 +1,7 @@
 <?php
-
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\GroupMenu;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 
@@ -15,38 +14,72 @@ class MejorasRutasPermission extends Seeder
      */
     public function run()
     {
+
+        $grupos = [
+            ['id' => '6', 'name' => 'Caja Grande', 'icon' => 'user_circle'],
+
+        ];
+
+        foreach ($grupos as $grupo) {
+            GroupMenu::updateOrCreate(['id' => $grupo['id']], $grupo);
+        }
+
         $permissions = [
             [
-                'id' => 31,
-                'name' => 'Unidades',
-                'route' => 'unidades',
-                'icon' => 'dot',
-                'guard_name' => 'web',
-                'groupMenu_id' =>1,
+                'id'           => 31,
+                'name'         => 'Unidades',
+                'route'        => 'unidades',
+                'icon'         => 'dot',
+                'guard_name'   => 'web',
+                'groupMenu_id' => 2,
             ],
             [
-                'id' => 32,
-                'name' => 'Tarifas',
-                'route' => 'tarifas',
-                'icon' => 'dot',
-                'guard_name' => 'web',
-                'groupMenu_id' => 1,
+                'id'           => 32,
+                'name'         => 'Tarifas',
+                'route'        => 'tarifas',
+                'icon'         => 'dot',
+                'guard_name'   => 'web',
+                'groupMenu_id' => 2,
             ],
             [
-                'id' => 33,
-                'name' => 'Productos',
-                'route' => 'productos',
-                'icon' => 'dot',
-                'guard_name' => 'web',
-                'groupMenu_id' => 1,
+                'id'           => 33,
+                'name'         => 'Productos',
+                'route'        => 'productos',
+                'icon'         => 'dot',
+                'guard_name'   => 'web',
+                'groupMenu_id' => 2,
             ],
             [
-                'id' => 34,
-                'name' => 'Almacén de Cargas',
-                'route' => 'almacencargas',
-                'icon' => 'dot',
-                'guard_name' => 'web',
-                'groupMenu_id' => 1,
+                'id'           => 34,
+                'name'         => 'Almacén de Cargas',
+                'route'        => 'almacencargas',
+                'icon'         => 'dot',
+                'guard_name'   => 'web',
+                'groupMenu_id' => 2,
+            ],
+            [
+                'id'           => 35,
+                'name'         => 'Bancos',
+                'route'        => 'bancos',
+                'icon'         => 'dot',
+                'guard_name'   => 'web',
+                'groupMenu_id' => 6,
+            ],
+            [
+                'id'           => 36,
+                'name'         => 'Conceptos Transacción',
+                'route'        => 'conceptostransaccion',
+                'icon'         => 'dot',
+                'guard_name'   => 'web',
+                'groupMenu_id' => 6,
+            ],
+            [
+                'id'           => 37,
+                'name'         => 'Cuentas Bancarias',
+                'route'        => 'cuentasbancarias',
+                'icon'         => 'dot',
+                'guard_name'   => 'web',
+                'groupMenu_id' => 6,
             ],
         ];
 
