56faa07e93fd300567827887642787f48b523d52
Miguel Guevara <guevaracajusolmiguel@gmail.com>
2025-08-20 21:54:45 -0500

    feat: se cambia nombre del abtal para la accion de l permiso

--- database/migrations/2025_08_20_190628_add_field_action_to_permissions.php
+++ database/migrations/2025_08_20_190628_add_field_action_to_permissions.php
@@ -13,9 +13,9 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::table('option_menus', function (Blueprint $table) {
+        Schema::table('option_Menus', function (Blueprint $table) {
             $table->string('action')->default('list')->after('id')->nullable();
-         $indexes = DB::select("SHOW INDEXES FROM option_menus WHERE Key_name = 'option_menus_name_guard_name_unique'");
+         $indexes = DB::select("SHOW INDEXES FROM option_Menus WHERE Key_name = 'option_menus_name_guard_name_unique'");
 
     if (!empty($indexes)) {
         $table->dropUnique('option_menus_name_guard_name_unique');
@@ -31,7 +31,7 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::table('option_menus', function (Blueprint $table) {
+        Schema::table('option_Menus', function (Blueprint $table) {
             $table->dropColumn('action');
         });
     }
